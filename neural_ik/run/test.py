bf6a34803b1b6961f8ef7f9d362d7b53f0b36f81
avlo2000 <pmykalov@gmail.com>
2022-09-20 15:44:45 +0300

    fixes

--- neural_ik/run/test.py
+++ neural_ik/run/test.py
@@ -8,6 +8,4 @@ from tf_kinematics.kinematic_models import kuka_robot
 if __name__ == '__main__':
     kin = kuka_robot()
     model = fk_dnn(kin)
-    print(kin.dof)
-    print(tf.random.uniform(shape=(1, kin.dof)))
     print(model(tf.random.uniform(shape=(1, kin.dof))))
